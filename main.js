d88639fc95777d140a6912b556f5e4651c732284
grzgm <g.malisz@student.fontys.nl>
2024-08-12 15:12:33 +0200

    sidebar animation

--- main.js
+++ main.js
@@ -2,19 +2,28 @@ const menuContainer = document.querySelector(".nav-content__menu-container");
 const sideBar = document.getElementById("side-bar");
 
 menuContainer.addEventListener("click", () => {
-  if (sideBar.style.display === "flex") {
-    sideBar.style.display = "none";
+  if (sideBar.classList.contains("side-bar--show")) {
+    sideBar.classList.remove("side-bar--show");
+    setTimeout(() => {
+      sideBar.style.display = "none";
+    }, 300); // Wait for animation to complete before hiding
   } else {
     sideBar.style.display = "flex";
+    setTimeout(() => {
+      sideBar.classList.add("side-bar--show");
+    }, 10); // Allow the display property to apply before animating
   }
 });
 
 document.addEventListener("click", (event) => {
   if (
-    sideBar.style.display === "flex" &&
+    sideBar.classList.contains("side-bar--show") &&
     !sideBar.contains(event.target) &&
     !menuContainer.contains(event.target)
   ) {
-    sideBar.style.display = "none";
+    sideBar.classList.remove("side-bar--show");
+    setTimeout(() => {
+      sideBar.style.display = "none";
+    }, 300); // Wait for animation to complete before hiding
   }
 });
